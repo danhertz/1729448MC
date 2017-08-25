5c858f70dcda9d3123492cdcd5a62d555f45e4b8
danhertz <30913814+danhertz@users.noreply.github.com>
2017-08-24 21:34:14 -0500

    minimo.py

---
+++ minimo.py
@@ -0,0 +1,25 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:57:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import random
+p=[]
+for i in range (100)
+        p.appendi(i)
+
+def minimo(arr):
+        aux = arr # creo un areglo auxiliar 
+        resultado = 0
+        for n1 in arr: # para cada elemento n1 del arregl/conjunto
+                aux.pop(n1) # remuevo n1 del conjunto auxiliar 
+                for n2 in aux: # para cada elemento n2 del conjunto auxiliar 
+                        print (n1,n2)
+                        if (n1 > n2): # comparo si n1 es mayor que n2 
+                                 break # si lo es, salgo para probar el siguiente n1
+siguente n2 
+                resultado =  n1 
+
+
+
+
+
+
+random.shuffle(p)
