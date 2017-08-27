9b465ef043e36b75611515928805581be267de53
danhertz <30913814+danhertz@users.noreply.github.com>
2017-08-27 17:46:17 -0500

    Add files via upload

--- minimo.py
+++ minimo.py
@@ -1,25 +1,70 @@
 Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:57:36) [MSC v.1900 64 bit (AMD64)] on win32
 Type "copyright", "credits" or "license()" for more information.
->>> import random
-p=[]
-for i in range (100)
-        p.appendi(i)
-
+>>> def minimo(arr):
+... x=arr[0]
+... for z in arr:
+... 	if(z<x):
+...		x=z
+... return z
+...
+>>>> def ordenar(arr):
+... 	arrsort=[]
+...	for z in range(len(arr)):
+...		z=minimo(arr)
+...		arr.remove(z)
+... 		arrsort.append(z)
+... return arrsort
+SyntaxError: expected an indented block
+>>> cnt=0
 def minimo(arr):
-        aux = arr # creo un areglo auxiliar 
-        resultado = 0
-        for n1 in arr: # para cada elemento n1 del arregl/conjunto
-                aux.pop(n1) # remuevo n1 del conjunto auxiliar 
-                for n2 in aux: # para cada elemento n2 del conjunto auxiliar 
-                        print (n1,n2)
-                        if (n1 > n2): # comparo si n1 es mayor que n2 
-                                 break # si lo es, salgo para probar el siguiente n1
-siguente n2 
-                resultado =  n1 
+    k=arr[0]
+    global cnt
+    for w in arr:
+        cnt+=1
+        if(w<k):
+            k=w
+    return k
 
+def ordenar(arr):
+    aux=arr[:]
+    arrsort=[]
+    for k in range(len(aux)):
+        w=minimo(aux)
+        aux.remove(w)
+        arrsort.append(w)
+    return arrsort
 
+import random
+p=random.sample(range(0,100),40)
+print(p)
+psort=ordenar(p)
+print(cnt)
+print(psort)
+SyntaxError: multiple statements found while compiling a single statement
+>>> cnt=0
+def minimo(arr):
+    z=arr[0]
+    global cnt
+    for x in arr:
+        cnt+=1
+        if(x<z):
+            z=x
+    return z
 
+def ordenar(arr):
+    aux=arr[:]
+    arrsort=[]
+    for z in range(len(aux)):
+        x=minimo(aux)
+        aux.remove(z)
+        arrsort.append(z)
+    return arrsort
 
-
-
-random.shuffle(p)
+import random
+p=random.sample(range(0,100),40)
+print(p)
+psort=ordenar(p)
+print(cnt)
+print(psort)
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
