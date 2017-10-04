e14c1048ecbf1455f90de128f1bbc04b95212ea3
danhertz <30913814+danhertz@users.noreply.github.com>
2017-10-03 19:05:08 -0500

    Add files via upload

---
+++ Primoclase.py
@@ -0,0 +1,7 @@
+primo(n):
+	cnt=0
+		for(i in range (2,round(n**0.5)):
+			cnt=cnt+1
+				if((n%i)==0): 	
+                                   break          
+                 return  cnt 																	
