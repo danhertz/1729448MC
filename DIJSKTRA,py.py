e74764f9281f1a3878b8351d1428422aec574b9a
danhertz <30913814+danhertz@users.noreply.github.com>
2017-10-29 18:16:45 -0600

    30 0ct
    30 0ct

--- DIJSKTRA,py.py
+++ DIJSKTRA,py.py
@@ -1,5 +1,21 @@
 from heapq import heappop,heappush
 from copy import deepcopy
+import random
+
+import time
+def permutation(lst):
+    if len(lst)==0:
+        return[]
+    if len(lst)==1:
+        return[lst]
+    l=[]#empty list that will store current permutation
+    for i in range(len(lst)):
+        m=lst[i]
+        remlst=lst[:i]+lst[i+1:]
+    for p in permutation(remlst):
+        l.append([m]+p)
+    return l
+
 
 class Fila:
     def __init__(self):
@@ -129,7 +145,22 @@ class Grafo:
         print('MST con peso', peso, ':', nuevo, '\n', arbol.E)
         return arbol
 
-g=Grafo
+    def vecinoMasCercano(self):
+        lv=list(self.V)
+        random.shuffle(lv)
+        ni=lv.pop()
+        le=dict()
+        while len(lv)>=0:
+            ln=self.v[ni]
+            for nv in ln:
+                le[nv]=self.E[(ni,nv)]
+            menor=min(le.values())
+            lv.append(menor)
+            del lv[menor]
+        return lv
+            
+
+g=Grafo()
 g.conecta('a','b', 1)
 g.conecta('a','c', 1)
 g.conecta('a','d', 1)
@@ -158,6 +189,11 @@ for r in range(10):
     c+=g.E[(dfs[-1],dfs[0])]
     print(dfs[-1],dfs[0],g.E[(dfs[-1],dfs[0])])
     print('costo',c)
+
+data=list('abcdefghij')
+tim=time.clock()
+per=permutation(data)
+print(time.clock()-tim)
 
                               
             
